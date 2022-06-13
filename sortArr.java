c1aa4587c7939cc62efd8a489c1880f7fd26cca7
AnjuVarghese12345 <93638518+AnjuVarghese12345@users.noreply.github.com>
2022-06-13 21:37:41 +0530

    Update sortArr.java

--- sortArr.java
+++ sortArr.java
@@ -1,14 +1,38 @@
-import java.util.Arrays;
+import java.util.Scanner;
 
-public class sortArr
+class stringSort 
 {
-  public static void main(String[] args)
-  {
-    String[] fruits = {"banana", "orange", "kiwi", "apple"};
-    Arrays.sort(fruits);
-    for (String fruit : fruits)
-    {
-      System.out.println(fruit);
-    }
-  }
+	public static void main(String[] args)
+	{
+		int n;
+		String words[]= new String[10],temp;
+		Scanner in = new Scanner(System.in);
+		System.out.println("Enter the number of strings:");
+		n = in.nextInt();
+		System.out.println("Enter the strings:");
+
+		for (int i =0 ; i < n ; i++)
+		{
+		 words[i] = in.next();
+		}
+
+		for (int i = 0; i < n; i++) 
+		{
+			for (int j = i + 1; j < n; j++) 
+			{
+				if (words[i].compareTo(words[j]) > 0) 
+				{
+					temp = words[i];
+					words[i] = words[j];
+					words[j] = temp;
+				}
+			}
+		}
+		
+		System.out.println("\nThe strings in alphabetical order are: ");
+		for (int i = 0; i < n; i++) 
+		{
+			System.out.println(words[i]);
+		}
+	}
 }
